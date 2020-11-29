eb46c0ffe94cdd5ad7cd3192e89280615673866d
Preston-Too <toopreston@gmail.com>
2020-11-30 01:19:21 +0300

    add sources class

--- app/models.py
+++ app/models.py
@@ -14,4 +14,15 @@ class News:
         self.urlToImage = urlToImage
 
 
+class Sources:
 
+    """
+    Source class to define the source of news
+    """
+    
+    def __init__(self, id, name, description, category, url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.category = category
+        self.url = url
