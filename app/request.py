4f2639391bc4e76602b02c696320aa0865ec41c1
Preston-Too <toopreston@gmail.com>
2020-11-30 07:13:59 +0300

    add function that process article results

--- app/request.py
+++ app/request.py
@@ -35,7 +35,6 @@ def get_news_category(category):
     return news_results
 
 
-
 def process_results(news_list):
     
     '''
@@ -60,3 +59,42 @@ def process_results(news_list):
         news_results.append(source_object)
 
     return news_results
+
+def get_news(category):
+
+    '''
+    This function gets the json response to url request
+    '''
+
+    get_news_url = article_url.format(category, apiKey)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_article_results(news_results_list)
+
+    return news_results
+
+def process_article_results(article_list):
+
+    article_results = []
+
+    for item in article_list:
+        author = item.get('author')
+        title = item.get('title')
+        description = item.get('description')
+        url = item.get('url')
+        urlToImage = item.get('urlToImage')
+        id = item.get('id')
+        publishedAt = item.get('publishedAt')
+
+        if url:
+            article_object = News(author,id, title, description, url, urlToImage,publishedAt)
+            article_results.append(article_object)
+
+    return article_results
